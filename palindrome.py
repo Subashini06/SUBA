f931b02f80c66d96b7840fbd37656232e608c5f3
Subashini06 <37295628+Subashini06@users.noreply.github.com>
2018-03-16 17:52:35 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,11 @@
 n=int(input("Enter number: "))
+t=n
 rev=0
 while(n>0):
     dig=n%10
     rev=rev*10+dig
     n=n//10
-print("Reverse of the number:",rev)
+if(t==rev):
+    print("Yes")
+    else:
+        print("No")
