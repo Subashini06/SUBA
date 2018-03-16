2995f52f4c29c4d7873edff1da13f5a340133cc8
Subashini06 <37295628+Subashini06@users.noreply.github.com>
2018-03-16 17:23:23 +0530

    Update finding numberof values.py

--- finding numberof values.py
+++ finding numberof values.py
@@ -1,6 +1,2 @@
-n=int(input("Enter number:"))
-count=0
-while(n>0):
-    count=count+1
-    n=n//10
-print("The number of digits in the number are:",count)
+number = int(input("Enter a number "))
+print ("Total number of digits : ",len(str(abs(number))))
